9b9c5c5ee28f636c2cef3c1cdddb304f5206be56
TJPlaysNow <pannebakertj@gmail.com>
2017-05-12 23:23:21 -0500

    Structure Configs
    Created configs for the structure and finished the method to go to and
    from build / move mode.

--- src/com/pzg/www/movingstructure/main/PluginMain.java
+++ src/com/pzg/www/movingstructure/main/PluginMain.java
@@ -1,9 +1,30 @@
 package com.pzg.www.movingstructure.main;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.pzg.www.api.config.Config;
 
 public class PluginMain extends JavaPlugin {
 	
+	public static Logger logger;
+	public static Plugin plugin;
+	public static Config config;
+	public static List<Config> structures;
 	
+	@Override
+	public void onEnable() {
+		logger = getLogger();
+		plugin = this;
+		config = new Config("plugins/StructureMover", "config.yml", plugin);
+		for (int i = 0; i <= config.getConfig().getInt("structures.ammount"); i++) {
+			String fileName = config.getConfig().getString("structure.file." + i);
+			Config structure = new Config("plugins/StructureMover/Structures", fileName + ".yml", plugin);
+			structures.add(structure);
+		}
+	}
 	
 }
--- src/com/pzg/www/movingstructure/main/objects/structure.java
+++ src/com/pzg/www/movingstructure/main/objects/structure.java
@@ -3,8 +3,11 @@ package com.pzg.www.movingstructure.main.objects;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Shulker;
 
@@ -15,6 +18,8 @@ public class structure {
 	protected List<Block> blocks = new ArrayList<Block>();
 	protected List<FallingBlock> fBlocks = new ArrayList<FallingBlock>();
 	protected List<Shulker> sBlocks = new ArrayList<Shulker>();
+	protected Location center;
+	protected ArmorStand centerStand;
 	
 	protected StructureState state;
 	
@@ -45,6 +50,42 @@ public class structure {
 		return name;
 	}
 	
+	public void setCenter(Location center) {
+		this.center = center;
+	}
+	
+	public void refreshCenter() {
+		World world = blocks.get(1).getWorld();
+		List<Integer> allX = new ArrayList<Integer>();
+		List<Integer> allY = new ArrayList<Integer>();
+		List<Integer> allZ = new ArrayList<Integer>();
+		for (Block block : blocks) {
+			allX.add(block.getX());
+			allY.add(block.getY());
+			allZ.add(block.getZ());
+		}
+		int addedX = 0;
+		for (int x : allX) {
+			addedX = addedX + x;
+		}
+		int addedY = 0;
+		for (int y : allY) {
+			addedY = addedY + y;
+		}
+		int addedZ = 0;
+		for (int z : allZ) {
+			addedZ = addedZ + z;
+		}
+		int finalX = addedX / allX.size();
+		int finalY = addedY / allY.size();
+		int finalZ = addedZ / allZ.size();
+		center = new Location(world, finalX, finalY, finalZ);
+	}
+	
+	public Location getCenter() {
+		return center;
+	}
+	
 	@SuppressWarnings("deprecation")
 	public void setState(StructureState state) {
 		if (state == StructureState.Build) {
@@ -59,6 +100,12 @@ public class structure {
 				fBlocks.remove(fBlock);
 				fBlock.remove();
 			}
+			for (Shulker sBlock : sBlocks) {
+				sBlocks.remove(sBlock);
+				sBlock.remove();
+			}
+			centerStand.remove();
+			
 		}
 		if (state == StructureState.Moving) {
 			if (this.state == StructureState.Moving) {
@@ -75,6 +122,14 @@ public class structure {
 				fBlocks.add(fBlock);
 				blocks.remove(block);
 				block.setType(Material.AIR);
+			}
+			centerStand = getCenter().getWorld().spawn(getCenter(), ArmorStand.class);
+			centerStand.setAI(false);
+			centerStand.setCollidable(false);
+			centerStand.setGravity(false);
+			centerStand.setVisible(false);
+			for (Shulker sBlock : sBlocks) {
+				centerStand.addPassenger(sBlock);
 			}
 		}
 	}
